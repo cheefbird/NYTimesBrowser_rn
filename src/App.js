16dac2568890d3321dbd44235cbf04ea46c37e66
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-05 18:48:40 -0700

    Implement basic StackNavigator following react navigation redux tutorial.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React, { Component } from "react";
+import { Provider, connect } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import {
+  reduxifyNavigator,
+  createReactNavigationReduxMiddleware
+} from "react-navigation-redux-helpers";
 import ReduxThunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducers from "./reducers";
-import TopNews from "./screens/TopNews";
+import AppNavigator from "./Routes";
 
-export default class App extends React.Component {
+const reactNavigationMiddleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav
+);
+
+const AppRoot = reduxifyNavigator(AppNavigator, "root");
+
+const mapStateToProps = state => ({
+  state: state.nav
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(AppRoot);
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(ReduxThunk, reactNavigationMiddleware))
+);
+
+export default class App extends Component {
   render() {
-    const store = createStore(
-      reducers,
-      composeWithDevTools(applyMiddleware(ReduxThunk))
-    );
-
     return (
       <Provider store={store}>
-        <TopNews />
+        <AppWithNavigationState />
       </Provider>
     );
   }
