0681f9d0585b5136f6ab65c9cd39ba12724b888f
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-06 19:06:08 -0700

    Create Routes.js

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from "react-navigation";
+
+import TopNews from "./screens/TopNews";
+import Article from "./screens/Article";
+
+export default createStackNavigator(
+  {
+    TopStories: {
+      screen: TopNews
+    },
+    Detail: {
+      screen: Article
+    }
+  },
+  {
+    initialRouteName: "TopStories"
+  }
+);
