8859df691ae356719eced5fe3691c2840fd1bb0e
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-06 19:25:37 -0700

    Remove react-navigation-redux-helpers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,7 @@
 import { combineReducers } from "redux";
-import { createNavigationReducer } from "react-navigation-redux-helpers";
-import AppNavigator from "../Routes";
 
 import TopNewsReducer from "./TopNewsReducer";
 
-const navReducer = createNavigationReducer(AppNavigator);
-
 export default combineReducers({
-  nav: navReducer,
   topNewsArticles: TopNewsReducer
 });
