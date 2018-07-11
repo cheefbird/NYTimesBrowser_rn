7fe901d8d8efa88dcf43e2f7c97167dcdfbf9f5e
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-10 23:35:51 -0700

    Finish FeaturedArticle and show in TopNews screen.

--- src/screens/TopNews.js
+++ src/screens/TopNews.js
@@ -4,8 +4,8 @@ import { StyleSheet, View, FlatList, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
 
 import { fetchTopNews } from "../actions";
-import StandardArticle from "../components/StandardArticle";
-// import FeaturedArticle from "../components/FeaturedArticle";
+// import StandardArticle from "../components/StandardArticle";
+import FeaturedArticle from "../components/FeaturedArticle";
 
 class TopNews extends PureComponent {
   static navigationOptions = {
@@ -24,33 +24,43 @@ class TopNews extends PureComponent {
     }
   };
 
+  createImageObject = mediaObject => {
+    if (mediaObject.length > 0) {
+      const thumbnailObject = mediaObject.find(
+        object => object.format === "Standard Thumbnail"
+      );
+
+      const jumboImageObject = mediaObject.find(
+        object => object.format === "superJumbo"
+      );
+
+      return { thumb: thumbnailObject.url, large: jumboImageObject.url };
+    } else {
+      return {
+        thumb:
+          "https://static01.nyt.com/images/2018/07/10/briefing/071018briefing-evening-promo1/071018briefing-evening-promo1-thumbStandard-v2.jpg",
+        large:
+          "https://static01.nyt.com/images/2018/07/02/briefing/03ambriefing-asia-SS-slide-4QC4/03ambriefing-asia-SS-slide-4QC4-thumbStandard.jpg"
+      };
+    }
+  };
+
   keyExtractor = (item, index) => index.toString();
 
   renderItem = ({ item }) => {
     const { multimedia } = item;
-    let thumbnailImageUrl;
 
-    if (multimedia.length > 0) {
-      const thumbnailObject = multimedia.find(
-        object => object.format === "Standard Thumbnail"
-      );
-      const { url } = thumbnailObject;
-
-      thumbnailImageUrl = url;
-    } else {
-      thumbnailImageUrl =
-        "https://static01.nyt.com/images/2018/07/02/briefing/03ambriefing-asia-SS-slide-4QC4/03ambriefing-asia-SS-slide-4QC4-thumbStandard.jpg";
-    }
+    const imageObject = this.createImageObject(multimedia);
 
     const sectionText = this.createSectionText(item.section, item.subsection);
     const { navigation } = this.props;
 
     return (
-      <StandardArticle
+      <FeaturedArticle
         title={item.title}
         subtitle={item.abstract}
         sectionText={sectionText}
-        imageUri={thumbnailImageUrl}
+        imageUri={imageObject.large}
         onPress={() =>
           navigation.push("Detail", {
             url: item.url
