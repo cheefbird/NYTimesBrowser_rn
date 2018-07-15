df250338a7e5f6651b9cb76ede2abdd7d03da44c
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-15 00:32:44 -0700

    Add SET_NEWS_CATEGORY action.

--- src/reducers/TopNewsReducer.js
+++ src/reducers/TopNewsReducer.js
@@ -1,11 +1,17 @@
-import { FETCH_TOP_NEWS } from "../actions/types";
+import { FETCH_TOP_NEWS, SET_NEWS_CATEGORY } from "../actions/types";
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  articles: [],
+  selectedCategory: "home"
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_TOP_NEWS:
-      return action.payload;
+      return { ...state, articles: action.payload };
+
+    case SET_NEWS_CATEGORY:
+      return { ...state, selectedCategory: action.payload };
 
     default:
       return state;
