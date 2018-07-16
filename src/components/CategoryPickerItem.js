1f8313efc1a18449af33d5d5f495318f1daa182c
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-16 13:52:39 -0700

    Switch picker to native-base component.

--- src/components/CategoryPickerItem.js
+++ src/components/CategoryPickerItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Picker } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Picker } from "native-base";
 import PropTypes from "prop-types";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
