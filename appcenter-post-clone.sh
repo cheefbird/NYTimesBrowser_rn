547ecedb27c798142dabb11fcd9441533663cb00
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-20 19:03:34 -0700

    Attempt again...

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -ex
-brew uninstall node
-NODE_VERSION="8.11.3"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+
+brew uninstall --force node
+brew cleanup --force -s node
+brew prune
+brew install node
+
+# brew uninstall node
+# NODE_VERSION="8.11.3"
+# curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+# sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
