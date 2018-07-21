3c349a7949f49a8b4706f70c1b22c676e967ea66
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-20 19:11:50 -0700

    AAAND AGAIN!

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-brew uninstall --force node
-brew cleanup --force -s node
-brew prune
+brew uninstall node@6
+
 brew install node
 
-echo 'export PATH="/usr/local/opt/icu4c/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="/usr/local/opt/icu4c/sbin:$PATH"' >> ~/.bash_profile
+brew link node
 # brew uninstall node
 # NODE_VERSION="8.11.3"
 # curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
