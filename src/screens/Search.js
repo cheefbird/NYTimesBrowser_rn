87f36ad125edc3e0a1c51288312dd22317ad8a18
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-22 16:39:07 -0700

    Create and implement SearchCard

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,17 +2,13 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text, TextInput } from "react-native";
 import { DatePicker } from "native-base";
 
+import SearchCard from "../components/SearchCard";
+
 export default class Search extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <Text>Search articles by keyword:</Text>
-          <TextInput />
-        </View>
-        <View>
-          <DatePicker />
-        </View>
+        <SearchCard />
       </View>
     );
   }
@@ -20,9 +16,8 @@ export default class Search extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  inputContainer: {
-    padding: 8
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#000"
   }
 });
