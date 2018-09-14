a3218120d0f18061f08dfaa7dab7336f3030cde0
Francis Breidenbach <fbreidenbach@gmail.com>
2018-09-14 00:00:53 -0700

    Add isSearching to existing reducer.

--- src/reducers/SearchReducer.js
+++ src/reducers/SearchReducer.js
@@ -1,6 +1,7 @@
 import { FETCH_SEARCH_RESULTS } from "../actions/types";
 
 const INITIAL_STATE = {
+  isSearching: false,
   docs: [],
   meta: {
     hits: 0
@@ -10,7 +11,10 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_SEARCH_RESULTS:
-      return action.payload;
+      return {
+        isSearching: false,
+        ...action.payload
+      };
 
     default:
       return state;
